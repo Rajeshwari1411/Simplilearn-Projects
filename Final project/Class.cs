69cfaea21ed8acf5801860808af4879c0605b5e3
Rajeshwari1411 <111351392+Rajeshwari1411@users.noreply.github.com>
2022-09-15 16:39:42 +0530

    Add files via upload

---
+++ Final project/Class.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+
+{
+    public interface ITeam
+    {
+        void Add(Player player);
+        void Remove(int playerId);
+        Player GetPlayerById(int playerId);
+        Player GetPlayerByName(string playerName);
+        List<Player> GetAllPlayers();
+    }
+    public class Player
+    {
+        private int _PlayerId;
+        private string _PlayerName;
+        private int _PlayerAge;
+        public int PlayerID
+        {
+            get { return _PlayerId; }
+            set { _PlayerId = value; }  
+        }
+        public string PlayerNAME
+        {
+            get { return _PlayerName; }
+            set { _PlayerName = value; }
+        }
+        public int PlayerAGE
+        {
+            get { return _PlayerAge; }
+            set { _PlayerAge = value; }
+        }
+       
+    }
+    public class OneDayTeam : Player, ITeam
+    {
+        
+        public void Add(Player player)
+        {
+            oneDayTeam.Add(player);
+        }
+        public void Remove(int PlayerId)
+        {
+            Player player = null;
+            foreach(var item in oneDayTeam)
+            {
+                if(item.PlayerID == PlayerId)
+                {
+                   player = item; 
+                }
+            }
+            OneDayTeam.Remove(player);
+            Console.WriteLine("Player removed successfully");
+        }
+
+        private static void Remove(Player player)
+        {
+            
+        }
+
+        public Player GetPlayerById(int playerId)
+        {
+            Player player = null;
+            foreach (var item in oneDayTeam)
+            {
+                if (item.PlayerID == playerId)
+                {
+                    player = item; break;
+                }
+            }
+            return player;
+
+        }
+
+        public Player GetPlayerByName(string PlayerName)
+        {
+            Player player = null;
+            foreach(var item in oneDayTeam)
+            {
+                if(item.PlayerNAME == PlayerName)
+                {
+                    player=item; break; 
+                }
+            }
+            return player;
+        }
+
+
+        public static List<Player> oneDayTeam = new List<Player>();
+        public OneDayTeam()
+        {
+            oneDayTeam.Capacity = 11;
+        }
+        public List<Player> GetAllPlayers()
+        {
+            return oneDayTeam;
+        }
+
+
+
+
+
+    }
+
+
+}
+
---
+++ Final project/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
+using System.Security.Cryptography;
+
+namespace PlayerAndTeamRequirement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            c1:
+            Console.WriteLine(" 1:To Add Player 2:To Remove Player by Id 3:Get Player By Id 4:Get Player by Name 5:Get All Players: ");
+            int i=Convert.ToInt32(Console.ReadLine());
+            switch(i)
+            {
+                    case 1:
+                    c2:
+                  
+                   OneDayTeam one=new OneDayTeam();
+                    if(OneDayTeam.oneDayTeam.Count !=OneDayTeam.oneDayTeam.Capacity)
+                    {
+                        Console.WriteLine("Enter the PlayerId:");
+                        one.PlayerID=Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the PlayerName:");
+                        one.PlayerNAME=Console.ReadLine();
+                        Console.WriteLine("Enter the PlayerAge:");
+                        one.PlayerAGE=Convert.ToInt32(Console.ReadLine());
+                        one.Add(one);
+                    }
+                    else
+                    {
+                        Console.WriteLine("The Team already contains 11 players");
+                        goto c1;
+                    }
+                    Console.WriteLine("Do you want to continue(yes/no)?:");
+                    string j=Console.ReadLine();
+                    if (j == "yes")
+                    {
+                        goto c2;
+                    }
+                    
+                    break;
+                    case 2:
+                    OneDayTeam day = new OneDayTeam();
+                    Console.WriteLine("Enter PlayerId to Remove:");
+                    int k=Convert.ToInt32(Console.ReadLine());
+                    day.Remove(k);
+                   
+                    break;
+                    case 3:
+                    OneDayTeam team = new OneDayTeam();
+                    Console.WriteLine("Enter the PlayerId to get:");
+                    int l=Convert.ToInt32(Console.ReadLine());  
+                    Player plr=team.GetPlayerById(l);
+                    Console.WriteLine($"{plr.PlayerID}{plr.PlayerNAME}{plr.PlayerAGE}");
+                    
+                    break;
+                    case 4:
+                    OneDayTeam OneDay=new OneDayTeam();
+                    Console.WriteLine("Enter the PlayerName to get:");
+                    string m=Console.ReadLine();
+                    Player ply=OneDay.GetPlayerByName(m);
+                    Console.WriteLine($"{ply.PlayerID}{ply.PlayerNAME}{ply.PlayerAGE}");
+                    
+                    break ;
+                    case 5:
+                    Console.WriteLine("Get all players in a team");
+                    List<Player> list=new List<Player>();
+                    OneDayTeam onedayteam = new OneDayTeam();
+                    list = onedayteam.GetAllPlayers();
+                    foreach(var item in list)
+                    {
+                        Console.WriteLine($"{item.PlayerID}{item.PlayerNAME}{item.PlayerAGE}");
+                    }
+                    break;
+                   
+
+                
+            }
+            Console.WriteLine("Do u want to start again?");
+            string n=Console.ReadLine();
+            if(n=="yes")
+            {
+                goto c1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
